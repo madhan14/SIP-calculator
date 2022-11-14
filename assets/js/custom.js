21b71d6508204091c9f7064ae705ad821aaba92d
Madhankumar <madhankumar05072000@protonmail.com>
2022-11-14 12:37:00 +0530

    :zap: form validation update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,27 +2,72 @@ function validate(){
     var amount = $('#amount').val();
     var time_period = $('#time_period').val();
     var returns = $('#returns').val();
+    var count = 0;
+    var data = [];
 
-    console.log(amount, time_period, returns)
-
-    if(amount == '' || amount < 100){
-        $('.invalid-feedback.amount').css('display', 'block');
+    if(amount < 100 || amount == ''){
+        $('.amount').css('display', 'block');
     } else {
-        $('.invalid-feedback.amount').css('display', 'none');
+        $('.amount').css('display', 'none');
+        
+        count = count + 1;
     }
 
-    if(time_period == '' || time_period < 3){
-        $('.invalid-feedback.time_period').css('display', 'block');
+    if(time_period < 3 || time_period == ''){
+        $('.time_period').css('display', 'block');
     } else {
-        $('.invalid-feedback.time_period').css('display', 'none');
+        $('.time_period').css('display', 'none');
+        count = count + 1;
     }
 
-    if(returns == '' || returns == 0){
-        $('.invalid-feedback.returns').css('display', 'block');
+    if(returns == ''){
+        $('.returns').css('display', 'block');
     } else {
-        $('.invalid-feedback.returns').css('display', 'none');
+        $('.returns').css('display', 'none');
+        count = count + 1;
+    }
+    
+    if(count == 3){
+        return 'pass';
     }
 }
+
+function amount_validate(){
+    var amount = $('#amount').val();
+    if(amount < 100 || amount == ''){
+        $('.amount').css('display', 'block');
+    } else {
+        $('.amount').css('display', 'none');
+    }
+}
+
+function time_period_validate(){
+    var time_period = $('#time_period').val();
+    if(time_period < 3 || time_period == ''){
+        $('.time_period').css('display', 'block');
+    } else {
+        $('.time_period').css('display', 'none');
+    }
+}
+
+function returns_validate(){
+    var returns = $('#amount').val();
+    if(returns == ''){
+        $('.returns').css('display', 'block');
+    } else {
+        $('.returns').css('display', 'none');
+    }
+}
+
+
 $('#submit').click(function() {
-        
+    if(validate() == 'pass'){
+        var amount = $('#amount').val();
+        var time_period = $('#time_period').val();
+        var returns = $('#returns').val();
+
+        console.log('Invest amount: '+amount);
+        console.log('Invest time_period: '+time_period);
+        console.log('returns: '+returns);
+    }
 });
